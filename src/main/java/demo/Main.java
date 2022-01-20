de8e33c58459882027c6d229280cc9290d3b04c2
SaiVamshiK <saivamshi.k.24@gmail.com>
2022-01-20 14:24:55 +0530

    added beans in spring.xml configuration file and used them in main class

--- src/main/java/demo/Main.java
+++ src/main/java/demo/Main.java
@@ -1,10 +1,18 @@
 package demo;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-        Doctor doctor = new Doctor();
+//        Doctor doctor = new Doctor();
+//        doctor.assist();
+//        Nurse nurse = new Nurse();
+//        nurse.assist();
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+        Doctor doctor = applicationContext.getBean(Doctor.class);
+        Nurse nurse = applicationContext.getBean(Nurse.class);
         doctor.assist();
-        Nurse nurse = new Nurse();
         nurse.assist();
     }
 }
