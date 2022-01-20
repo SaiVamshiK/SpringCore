4fb38d7c5f5e65d953778d54349f7c37c9459b58
SaiVamshiK <saivamshi.k.24@gmail.com>
2022-01-20 14:18:25 +0530

    created objects using new

--- src/main/java/demo/Main.java
+++ src/main/java/demo/Main.java
@@ -2,6 +2,9 @@ package demo;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Doctor doctor = new Doctor();
+        doctor.assist();
+        Nurse nurse = new Nurse();
+        nurse.assist();
     }
 }
