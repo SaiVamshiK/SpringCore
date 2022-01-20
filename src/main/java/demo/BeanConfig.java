31c7db312affe9265eebfe9f0b9cc51a26dc8bf1
SaiVamshiK <saivamshi.k.24@gmail.com>
2022-01-20 14:58:37 +0530

    corrected singleton configuration

--- src/main/java/demo/BeanConfig.java
+++ src/main/java/demo/BeanConfig.java
@@ -4,9 +4,11 @@ package demo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
-@Configuration("singleton")
+@Configuration
 @ComponentScan(basePackages = "demo")
+@Scope("singleton")
 public class BeanConfig {
     @Bean
     public Doctor doctor()
